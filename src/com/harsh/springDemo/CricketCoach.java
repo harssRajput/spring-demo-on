dc0dc873299e7fb97b403b17420da031af321cb0
Harsh Rajput <hrrajput20@gmail.com>
2022-10-12 11:30:04 +0530

    add literals and literals injection using setter in cricketCoach

--- src/com/harsh/springDemo/CricketCoach.java
+++ src/com/harsh/springDemo/CricketCoach.java
@@ -4,6 +4,10 @@ public class CricketCoach implements Coach {
 	
 	private FortuneService fortuneService = new HappyFortuneService();
 	
+	private String emailAddress;
+	private String team;
+	
+
 	//define a constructor for dependency injection aka constructor-injecting.
 	public CricketCoach(FortuneService fortuneService) {
 		System.out.println("CricketCoach: inside parameterised constructor");
@@ -14,8 +18,26 @@ public class CricketCoach implements Coach {
 		System.out.println("CricketCoach: inside no-arg constructor");
 	}
 	
+	public String getEmailAddress() {
+		return emailAddress;
+	}
+	
+	public void setEmailAddress(String emailAddress) {
+		System.out.println("CricketCoach: inside setter - setEmailAddress");
+		this.emailAddress = emailAddress;
+	}
+	
+	public String getTeam() {
+		return team;
+	}
+	
+	public void setTeam(String team) {
+		System.out.println("CricketCoach: inside setter - setTeam");
+		this.team = team;
+	}
+	
 	public void setFortuneService(FortuneService fortuneService) {
-		System.out.println("CricketCoach: inside setter setFortuneService");
+		System.out.println("CricketCoach: inside setter - setFortuneService");
 		
 		this.fortuneService = fortuneService;
 	}
